a3f36a995baeba43e1972978d51c40e8425b4bdc
pgsheng <pgsheng09@163.com>
2018-12-25 16:35:37 +0800

    优化

--- 8-Url.js
+++ 8-Url.js
@@ -1,17 +1,29 @@
 /*
-一个函数可以作为另一个函数的参数。我们可以先定义一个函数，然后传递，也可以在传递参数的地方直接定义函数。
-* */
-function say(word) {
-  console.log(word);
-}
-function execute(someFunction, value) {
-  someFunction(value);
-}
-execute(say, "Hello"); // 把say函数作为execute函数的第一个变量传递。这里传递不是say返回值，而是 say 本身
+要为路由提供请求的 URL 和其他需要的 GET 及 POST 参数，随后路由需要根据这些数据来执行相应的代码
+我们需要的所有数据都会包含在 request 对象中，该对象作为 onRequest() 回调函数的第一个参数传递。
+但是为了解析这些数据，我们需要额外的 Node.JS 模块，它们分别是 url 和 querystring 模块
+*/
 
 
-/*--------2、匿名函数*/
-function execute2(someFunction, value) {
-  someFunction(value);
+var http = require("http");
+var url = require("url");
+
+function start() {
+    //  onRequest() 函数加上一些逻辑，用来找出浏览器请求的 URL 路径
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World2");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
 }
-execute2(function(word){ console.log(word) }, "Hello2");
+exports.start = start;
+
+function route(pathname) {
+console.log("About to route a request for " + pathname);
+}
+exports.route = route;
