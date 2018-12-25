6f4acded7dc8622c4cb1de90d38ec5ce47af620b
pgsheng <pgsheng09@163.com>
2018-12-25 15:27:39 +0800

    优化

---
+++ 6-Main.js
@@ -0,0 +1,12 @@
+/*
+模块是Node.js 应用程序的基本组成部分，文件和模块是一一对应的。换言之，一个 Node.js 文件就是一个模块，
+这个文件可能是JavaScript 代码、JSON 或者编译过的C/C++ 扩展。
+* */
+// var hello = require('./hello'); // 引入了当前目录下的 hello.js文件,默认后缀为 js
+// hello.world();
+
+
+var Hello2 = require('./hello');
+hello2 = new Hello2();
+hello2.setName('BYVoid');
+hello2.sayHello();
