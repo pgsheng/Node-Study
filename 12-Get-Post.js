338180dc1277f03596fdd8c609fdcff814e252a7
pgsheng <pgsheng09@163.com>
2018-12-26 17:55:12 +0800

    优化

--- 12-Get-Post.js
+++ 12-Get-Post.js
@@ -19,3 +19,18 @@ http.createServer(function (req, res) {
     res.write("网站 URL：" + params.url);
     res.end();
 }).listen(3333); // 访问 http://localhost:3333/user?name=胜&url=www.baidu.com
+
+
+var querystring = require('querystring');
+http.createServer(function(req, res){
+    var post = '';       // 定义了一个post变量，用于暂存请求体的信息
+    // 通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
+    req.on('data', function(chunk){
+        post += chunk;
+    });
+    // 在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
+    req.on('end', function(){
+        post = querystring.parse(post);
+        res.end(util.inspect(post));
+    });
+}).listen(3000);
