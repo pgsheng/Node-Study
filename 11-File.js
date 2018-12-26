fe347a26366d166b177f5e879326ed279c273b2a
pgsheng <pgsheng09@163.com>
2018-12-26 16:29:36 +0800

    优化

--- 11-File.js
+++ 11-File.js
@@ -17,3 +17,45 @@ fs.readFile('./data/input.txt', function (err, data) {
 var data = fs.readFileSync('./data/input.txt');
 console.log("同步读取: " + data.toString());
 console.log("程序执行完毕。");
+
+// 异步打开文件
+// r+ 读写打开文件,文件不存在抛出异常;w+ 读写 打开文件,文件不存在则创建;a+ 读取追加打开文件,文件不存在则创建
+fs.open('./data/input.txt', 'r+', function(err, fd) {
+   if (err) {
+       return console.error(err);
+   }
+  console.log("文件打开成功！");
+});
+
+// 获取文件信息
+fs.stat('./data/input.txt', function (err, stats) {
+    // console.log(stats); // 文件信息
+    console.log('是否为文件:' + stats.isFile());  //true
+});
+
+// 写入文件,a追加，w覆盖
+// fs.writeFile('./data/output.txt', '我是通过fs.writeFile 写入文件的内容', {encoding:'utf8',flag:'a'}, function(err) {
+//    if (err) {
+//        return console.error(err);
+//    }
+//    console.log("数据写入成功！");
+// });
+
+// 读取文件
+var buf = new Buffer.alloc(1024);
+fs.open('./data/output.txt', 'r+', function(err, fd) {
+   if (err) {
+       return console.error(err);
+   }
+   fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+      if (err){
+         console.log(err);
+      }
+      console.log(bytes + "  字节被读取");
+
+      // 仅输出读取的字节
+      if(bytes > 0){
+         console.log(buf.slice(0, bytes).toString());
+      }
+   });
+});
