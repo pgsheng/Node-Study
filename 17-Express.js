31dd7b6027851c872edb6e250ee1610b2730e2ab
pgsheng <pgsheng09@163.com>
2018-12-28 15:05:52 +0800

    优化

--- 17-Express.js
+++ 17-Express.js
@@ -6,4 +6,7 @@ Express 框架核心特性：
 定义了路由表用于执行不同的 HTTP 请求动作。
 可以通过向模板传递参数来动态渲染 HTML 页面。
 $ cnpm install express --save
+$ cnpm install body-parser --save   // 中间件，用于处理 JSON, Raw, Text 和 URL 编码的数据
+$ cnpm install cookie-parser --save // 解析Cookie的工具。req.cookies取到传过来的cookie，并把它们转成对象
+$ cnpm install multer --save // 中间件，用于处理 enctype="multipart/form-data" 的表单数据
 */
