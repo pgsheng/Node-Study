119680e1c151e98e23bd18e536ec50a9778cfb58
pgsheng <pgsheng09@163.com>
2019-01-02 17:52:40 +0800

    优化

--- 21-MySQL.js
+++ 21-MySQL.js
@@ -4,5 +4,5 @@ Xcore 是一个支持多线程的 Node.js 发行版本，基本不需要对你�
 2、解压到某个目录，并配置系统环境变量：jx --version ，查看是否成功
 3、进入项目目录（必要），使用 jx 命令打包项目，并指定项目的主文件，比如，index.js
    jx package index.js index   （最后一个是打包生成文件的命名，自定义）
-4、运行：jx index.jx
+4、
 */
