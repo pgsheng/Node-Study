d4384cc3146ba3ca411885c8f66fe01e69587600
pgsheng <pgsheng09@163.com>
2019-01-03 10:18:36 +0800

    优化

--- 20-JXcore.js
+++ 20-JXcore.js
@@ -5,4 +5,5 @@ Xcore 是一个支持多线程的 Node.js 发行版本，基本不需要对你�
 3、进入项目目录（必要），使用 jx 命令打包项目，并指定项目的主文件，比如，index.js
    jx package index.js index   （最后一个是打包生成文件的命名，自定义）
 4、运行：jx index.jx
+5、第三方依赖打包有问题
 */
---
+++ 22-MongoDB.js
@@ -0,0 +1,3 @@
+/*
+$ cnpm install mysql
+*/
