c97a8e9a86aedf576e15c51ccfacb6f279792e5d
pgsheng <pgsheng09@163.com>
2019-01-03 10:58:52 +0800

    优化

--- 21-MySQL.js
+++ 21-MySQL.js
@@ -1,3 +1,56 @@
 /*
-$ cnpm install mongodb
+$ cnpm install mysql
 */
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306, // 默认：3306
+    user: 'root',
+    password: '123456',
+    database: 'teacherdb'
+});
+
+connection.connect();
+
+var sql = "SELECT * FROM teacher";
+connection.query(sql, function (err, result) {
+    if (err) {
+        console.log('[SELECT ERROR] - ', err.message);
+    } else {
+        console.log(result);
+    }
+});
+
+var addSql = 'INSERT INTO teacher(Id,name,age)VALUES(0,?,?)';
+var addSqlParams = ['小龙', '99'];
+connection.query(addSql, addSqlParams, function (err, result) {
+    if (err) {
+        console.log('[INSERT ERROR] - ', err.message);
+        return;
+    }
+    console.log('--------------------------INSERT----------------------------');
+    console.log('INSERT ID:', result);
+});
+
+var modSql = 'update teacher set age = ? ,name = ? where id = ?';
+var modSqlParams = ['51', '小明', '39'];
+connection.query(modSql, modSqlParams, function (err, result) {
+    if (err) {
+        console.log('[UPDATE ERROR] - ', err.message);
+        return;
+    }
+    console.log('--------------------------UPDATE----------------------------');
+    console.log('UPDATE affectedRows', result.affectedRows);
+});
+
+var delSql = "DELETE FROM teacher where name='小红2'";
+connection.query(delSql,function (err, result) {
+        if(err){
+          console.log('[DELETE ERROR] - ',err.message);
+          return;
+        }
+       console.log('--------------------------DELETE----------------------------');
+       console.log('DELETE affectedRows',result.affectedRows);
+});
+
+connection.end();
--- 22-MongoDB.js
+++ 22-MongoDB.js
@@ -1,3 +1,11 @@
 /*
-$ cnpm install mysql
+$ cnpm install mongodb
 */
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/testdb";
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    console.log("数据库已创建!");
+    db.close();
+});
