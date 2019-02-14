b806d5677ba7aa84aeb08bbef007a03f0f590ea6
pgsheng <pgsheng09@163.com>
2019-02-14 10:44:16 +0800

    优化

--- 6-Module1.js
+++ 6-Module1.js
@@ -20,3 +20,11 @@ function Hello() {
     };
 }
 module.exports = Hello;
+
+/**
+通过exports和module.exports对外公开的方法都可以访问，但有区别
+module.exports才是真正的接口，exports只不过是它的一个辅助工具。　最终返回给调用的是module.exports而不是exports。
+所有的exports收集到的属性和方法，都赋值给了Module.exports。当然，这有个前提，就是module.exports本身不具备任何属性和方法。
+如果，module.exports已经具备一些属性和方法，那么exports收集来的信息将被忽略
+ NodeJs开发者建议导出对象用module.exports,导出多个方法和变量用exports
+*/
--- 8-Url.js
+++ 8-Url.js
@@ -32,3 +32,10 @@ console.log("About to route a request for " + pathname);
 start(route);
 
 // 浏览器访问 http://127.0.0.1:8888
+
+
+/**
+ * req.query: 解析后的 url 中的 querystring，如 ?name=haha，req.query 的值为 {name: 'haha'}
+req.params: 解析 url 中的占位符，如 /:name，访问 /haha，req.params 的值为 {name: 'haha'}
+req.body: 解析后请求体，需使用相关的模块，如 body-parser，请求体为 {"name": "haha"}，则 req.body 为 {name: 'haha'}
+ * */
